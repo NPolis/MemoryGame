ad9fff4c43c1b015b302b8390a15ab52c108a3a0
NPolis <polishchuknadezhda93@gmail.com>
2025-03-31 22:23:56 +0100

    First Commit

---
+++ main.js
@@ -0,0 +1,60 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let card1 = ""
+    let card2 = ""
+    let cards = document.getElementsByClassName("flip-card")
+    for (let card of cards) {
+        card.addEventListener("click", function (event) {
+            //event.stopPropagation()
+            let target = event.target.parentElement.parentElement
+
+
+            if (target.tagName === "LI") {
+
+                console.log(card1)
+                console.log(card2)
+                target.classList.add("select")
+                if (card1 === "") {
+                    card1 = target
+
+                } else {
+                    card2 = target
+                    setTimeout(matchCheck, 800)
+
+
+                }
+
+            }
+
+
+        }, true)
+    }
+    function resetCards() {
+        card1 = ""
+        card2 = ""
+    }
+    function matchCheck() {
+
+        if (card1.children[0].children[1].children[0].classList.value === card2.children[0].children[1].children[0].classList.value) {
+            card1.classList.add("match")
+            card2.classList.add("match")
+
+        } else {
+            card1.classList.remove("select")
+            card2.classList.remove("select")
+
+        }
+        resetCards()
+        winCheck()
+    }
+    function winCheck() {
+        let matchedCards = document.getElementsByClassName("match")
+        if (matchedCards.length === 16) {
+            console.log("you win!")
+        }
+    }
+})
+
+
+
+
+
